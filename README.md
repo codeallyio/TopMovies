fabc4448c51c138e82d923b92565afb453e24aea
Piotr Jasiński <jasinskipiotr99@gmail.com>
2020-08-27 17:51:33 +0200

    candidate own test possibility

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-#### Find the best movies among movie platform <br>
+### Find the best movies among movie platform
 Implement a method that will analyze movies dataset and return array of <b>5</b> best rated movies.
 You can assume that given dataset contains at least 5 movies. Movies in returned array have to be sorted
 starting from the best rated movie.
+
+#### Test your solution
+You can run your program on example data to check if it compiles and works properly.
+Just click right on file name and then run.
+
+### Testing candidate solution 
+
+To check candidate solution, run in project directory:
+```
+mvn test
+```
--- src/main/java/io/strove/topmovies/Main.java
+++ src/main/java/io/strove/topmovies/Main.java
@@ -1,29 +1,22 @@
 package io.strove.topmovies;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     // TODO implement this method
-    public List<Movie> getBestMovies(List<Movie> moviesList) {
-        Collections.sort(moviesList, (movie, movie1) -> movie.getRating() < movie1.getRating() ? 1 : movie.getRating() == movie1.getRating() ? 0 : -1);
-        return moviesList.subList(0, 5);/*
-        Movie movies[] = new Movie[6];
-        for (int i = 0; i < moviesList.size(); i++) {
-            int pos = Math.min(i, 5);
-            movies[pos] = moviesList.get(i);
-            for (int j = pos; j > 0; j--) {
-                if (movies[j].getRating() > movies[j - 1].getRating()) {
-                    Movie tmpMovie = movies[j - 1];
-                    movies[j - 1] = movies[j];
-                    movies[j] = tmpMovie;
-                } else {
-                    break;
-                }
-            }
-        }
-        return Arrays.asList(movies).subList(0, 5);*/
+    public static List<Movie> getBestMovies(List<Movie> moviesList) {
+        return null;
+    }
+
+    // Run this code
+    public static void main (String[] args) {
+        List<Movie> movies = Arrays.asList(new Movie("Star Wars - The Phantom Menace", 4.2),
+                new Movie("Star Wars - Attack of the Clones", 4.9), new Movie("Star Wars - Revenge of the Sith", 3.5),
+                new Movie("Star Wars - A New Hope", 3.7), new Movie("Star Wars - The Empire Strikes Back", 4.6),
+                new Movie("Star Wars - Return of the Jedi", 4.3));
+        List<Movie> bestMovies = getBestMovies(movies);
+        for (Movie movie : bestMovies)
+            System.out.println(movie.getRating() + " " + movie.getTitle());
     }
 }
